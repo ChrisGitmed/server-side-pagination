4089e84ca0da32b12218f9119a3972c86c37ca5b
Chris Gitmed <cgitmed@gmail.com>
2021-12-13 12:29:05 -0800

    Closer

--- app/scripts/seedUsers.js
+++ app/scripts/seedUsers.js
@@ -9,20 +9,19 @@ const { db } = require('../db');
  */
 const seedUsers = async run => {
 
-  const template = { name: 'Test User' };
-
   if (!run) {
-    console.log(`\nDRY RUN, would have added 1000 dummy Users to the DB.`)
+    console.log(`\nDRY RUN, would have seeded 1000 dummy Users to the DB.`)
     console.log("USE --run TO SEED USERS\n")
   } else {
     console.log(`\nREAL RUN, creating 1000 Users...`);
 
     let successCount = 0;
     for (let i = 0; i < 1000; i++) {
+      const template = { name: `Test User ${i + 1}` };
       try {
         await db('users').insert(template);
         successCount++;
-      } catch(e) { console.log('Users.insert error: ', e)}
+      } catch(e) { console.log('users.insert error: ', e)}
     }
 
     console.log(`\n${successCount} Users created.`)
