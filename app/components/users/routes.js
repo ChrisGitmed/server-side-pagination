1a6eff38ab97e16732bf52755bb0acfc1b2bc776
Chris Gitmed <cgitmed@gmail.com>
2021-12-13 12:53:42 -0800

    cleanup

--- app/components/users/routes.js
+++ app/components/users/routes.js
@@ -13,24 +13,29 @@ router.get('/v1/users', async (req, res, next) => {
 });
 
 
-// Send back { data: [], current_page, last_page }
 /// Paginate (via limit and offset) 🎉
 router.get('/v1/pagination/users', async (req, res, next) => {
   let { page, limit } = req.query;
 
   if (!page) page = 1
   if (!limit) limit = 50;
-
   limit = parseInt(limit);
+
   const skip = (page - 1) * limit;
 
-  const [err, results] = await asCallBack(Users.getAllPaginated(skip, limit))
-  if (err) return errHandler(err, next, `Users.getAllPaginate: ${skip}, ${limit}`);
+  // Get Users
+  const [errUsers, users] = await asCallBack(Users.getAllPaginated(skip, limit))
+  if (errUsers) return errHandler(errUsers, next, `Users.getAllPaginated: ${skip}, ${limit}`);
 
-  const [errLast, last_page_results] = await asCallBack(Users.getLastPageNumber(limit));
+  // Get Last Page Number
+  const [errLast, last_page_number] = await asCallBack(Users.getLastPageNumber(limit));
   if (errLast) return errHandler(errLast, next, `Users.getLastPageNumber: ${limit}`);
-  console.log('last_page_results: ',last_page_results)
 
-
-  res.status(200).json({ data: results, current_page: parseInt(page), last_page: last_page_results, limit });
+  const resultPayload = {
+    data: users,
+    current_page_number: parseInt(page),
+    last_page_number,
+    limit
+  };
+  res.status(200).json(resultPayload);
 });
